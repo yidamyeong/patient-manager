ce65ae0b503fe12cdfc30d34f4323ea3145fde1b
2dam0 <yidamyeong@gmail.com>
2022-05-15 23:17:42 +0900

    [Feat] SpringBootApplication 생성

---
+++ src/main/java/com/demi/hdjassignment/PatientManagerApplication.java
@@ -0,0 +1,13 @@
+package com.demi.hdjassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientManagerApplication.class, args);
+    }
+
+}
