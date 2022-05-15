653e88a9d4612118279319546fdc65a601339cd0
2dam0 <yidamyeong@gmail.com>
2022-05-16 02:19:10 +0900

    [Feat] Application 구동과 함께 DB에 Init 데이터 삽입

---
+++ src/main/java/com/demi/hdjassignment/init/InitApplicationRunner.java
@@ -0,0 +1,80 @@
+package com.demi.hdjassignment.init;
+
+import com.demi.hdjassignment.entity.Hospital;
+import com.demi.hdjassignment.entity.Patient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InitApplicationRunner implements ApplicationRunner {
+
+    private final InitService initService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        initService.initDatabase();
+    }
+
+}
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+class InitService {
+
+    private final EntityManager entityManager;
+
+    public void initDatabase() {
+        List<Hospital> hospitals = initHospital();
+        List<Patient> patients = initPatient(hospitals);
+    }
+
+    private List<Hospital> initHospital() {
+        for (int i = 0; i < 10; i++) {
+            Hospital hospital = Hospital.builder()
+                            .name("Hospital Name - " + (i + 1))
+                            .instId(UUID.randomUUID().toString().substring(0, 8).toUpperCase())
+                            .director("D - " + (i + 1))
+                            .build();
+            entityManager.persist(hospital);
+        }
+
+        return entityManager
+                .createQuery(
+                        " select h from Hospital h" +
+                           " order by h.id asc", Hospital.class)
+                .getResultList();
+    }
+
+    private List<Patient> initPatient(List<Hospital> hospitals) {
+        for (Hospital hospital : hospitals) {
+            for (int i = 0; i < 20; i++) {
+                Patient patient = Patient.builder()
+                        .hospital(hospital)
+                        .name("Patient - " + (i + 1))
+                        .genderCode(i < 5 ? "F" : "M")
+                        .birth("yyyy-MM-dd")
+                        .mobile("010-1234-56" + (i < 10 ? "0" : "") + i)
+                        .build();
+                entityManager.persist(patient);
+            }
+        }
+
+        return entityManager
+                .createQuery(
+                        " select p from Patient p " +
+                           " order by p.id asc ", Patient.class)
+                .getResultList();
+    }
+
+}
